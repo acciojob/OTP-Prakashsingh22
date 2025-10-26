62c36b84cf352b9a8b7b97638a2fd96c53c96b9e
Prakashsingh22 <109896446+Prakashsingh22@users.noreply.github.com>
2025-10-26 17:43:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+const codes = document.querySelectorAll('.code');
+codes[0].focus();
+codes.forEach((input, index) => {
+      input.addEventListener('input', (e) => {
+        const value = e.target.value;
+        // Allow only one digit
+        if (!/^[0-9]$/.test(value)) {
+          e.target.value = '';
+          return;
+        }
+
+        // Move to the next input if available
+        if (index < codes.length - 1) {
+          codes[index + 1].focus();
+        }
+      });
+});
